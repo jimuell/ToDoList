6bcef89f93ef6a78e47e13e96c2ec10e6c68a609
jimuell <jimsswell@gmai.com>
2020-08-13 16:45:22 +0800

    Complete Code, Deployed using heroku and mongoDB atlas

--- app.js
+++ app.js
@@ -10,8 +10,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.listen(3000, function() {
-    console.log("Server is running on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
+app.listen(port, function() {
+    console.log("Server has started successfully");
 });
 
 
